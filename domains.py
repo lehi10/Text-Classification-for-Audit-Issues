42bbf4e29afa37b404c8b7062cea92e677638b1b
naivelogic <threetimberland@gmail.com>
2018-08-01 21:07:10 -0700

    Update domains.py

--- domains.py
+++ domains.py
@@ -1,7 +1,4 @@
 # domains.py
-"""
-Microsoft Security Policy Procedures (MSPP) from here: https://liquid.microsoft.com specifically: https://liquid.microsoft.com/Web/Views/View/69535
-"""
 
 domains = ['Operations Security','Cryptography','Systems Acquisition Development and Maintenance', 
 'Access Control','Asset Management','Physical and Environmental Security','Organization of Information Security','Business Continuity Management',
